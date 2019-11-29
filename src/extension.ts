bbabfb2ef31b08505595d5ae008335b3197bb1f4
whyour <imwhyour@gmail.com>
2019-11-29 17:08:03 +0800

    first commit

---
+++ src/extension.ts
@@ -0,0 +1,12 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	console.log('Congratulations, your extension "pdf-viewer" is now active!');
+	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+		vscode.window.showInformationMessage('Hello World!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+export function deactivate() {}
